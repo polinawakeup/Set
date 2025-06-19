d04aafa034464b7e09822bb9be62174952c740f6
Polina <polinawakeup@yandex.ru>
2025-06-19 19:33:11 +0300

    dependencies uninstalled

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-const a = () => 5 / 2;
+import Team from './set';
---
+++ src/set.js
@@ -0,0 +1,20 @@
+export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(character) {
+    if (this.members.has(character)) {
+      throw new Error('Выбранный персонаж уже есть в команде');
+    }
+    this.members.add(character);
+  }
+
+  addAll(...characters) {
+    characters.forEach((character) => this.members.add(character));
+  }
+
+  toArray() {
+    return Array.from(this.members);
+  }
+}
